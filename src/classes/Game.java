2125b9025eb27b9f48cc65dee9170e76b15da84b
LuisOlvera <luis.olvera@tralix.com>
2015-02-09 13:10:35 -0600

    kata bowling

--- src/classes/Game.java
+++ src/classes/Game.java
@@ -35,25 +35,15 @@ public class Game {
     }
 
 
-    /*
-     * @param indexRoll el indice del roll que se desea conocer si es Strike
-     */
     public boolean isStrike(int indexRoll){
         return rolls[indexRoll] ==10;
     }
 
-    /*
-    * @param indexRoll el indice del primer roll del frame del que se desea conocer si es Spare
-    */
     public boolean isSpare(int indexRoll)
     {
         return rolls[indexRoll] + rolls[indexRoll +1] >=10 ;
     }
 
-   /*
-   * @param indexRoll el indice de roll del frame que genera el Strike
-   * @param indexFrame el indice  del Frame del que se calculara el bono
-   */
     public  int addStrikeBono(int indexRoll, int indexFrame) {
         if (isFinalframe(indexFrame))
             return addFinalFrameWithStrike(indexRoll);
@@ -72,23 +62,14 @@ public class Game {
             return  10+ rolls[indexRoll +2] + rolls[indexRoll +3];
     }
 
-    /*
-    * @param indexFrame el indice del Frame
-    */
     public boolean isFinalframe (int indexFrame){
         return indexFrame ==9;
     }
 
-    /*
-    * @param indexRoll el indice del primer roll del Frame
-    */
     public int addWithoutBono(int indexRoll){
         return rolls[indexRoll] + rolls[indexRoll +1];
     }
 
-    /*
-    * @param indexRoll el indice del primer roll del Frame genera el spare
-    */
     public  int addSpareBono(int indexRoll){
         return  10 +rolls[indexRoll +2];
     }
