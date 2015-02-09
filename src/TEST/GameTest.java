7d944b83da93885027d06f1a480eb3026c974b68
LuisOlvera <luis.olvera@tralix.com>
2015-02-09 13:04:36 -0600

    kata bowling

--- src/TEST/GameTest.java
+++ src/TEST/GameTest.java
@@ -9,7 +9,7 @@ public class GameTest {
 
 int score=0;
     @org.junit.Test
-    public void gameTestUnsolopino ()throws Exception {
+    public void allOnes_Score20 ()throws Exception {
 
         Game g= new Game();
         for (int n=0;n<20; n++)
@@ -22,67 +22,100 @@ int score=0;
     }
 
     @org.junit.Test
-    public void gameTestSpare ()throws Exception {
-
+    public void oneSpareAllnextZero_Score19 ()throws Exception {
         Game g= new Game();
-
-
         g.roll(1);
         g.roll(9);
+
         g.roll(4);
         g.roll(1);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+        g.roll(0);
 
         score=g.score();
         Assert.assertEquals(score, 19);
     }
+
     @org.junit.Test
-    public void gameTestStrike ()throws Exception {
-
+    public void oneStrikeAllnextZero_Score28 ()throws Exception {
         Game g= new Game();
-
-
         g.roll(10);
-        g.roll(0);
+
         g.roll(4);
         g.roll(5);
 
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+        g.roll(0);
 
         score=g.score();
         Assert.assertEquals(score, 28);
     }
 
     @org.junit.Test
-    public void gameTestPerfect ()throws Exception {
-
+    public void allStrike_Score300 ()throws Exception {
         Game g= new Game();
-
+        g.roll(10);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
-
-        g.roll(10);
-        g.roll(0);
 
         g.roll(10);
         g.roll(10);
@@ -93,35 +126,25 @@ int score=0;
     }
 
     @org.junit.Test
-    public void gameTestultimoFramespark ()throws Exception {
+    public void nineStrikewithFinalSpare_score270 ()throws Exception {
         Game g= new Game();
+        g.roll(10);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
 
         g.roll(10);
-        g.roll(0);
-
-        g.roll(10);
-        g.roll(0);
 
         g.roll(0);
         g.roll(10);
@@ -132,41 +155,183 @@ int score=0;
     }
 
     @org.junit.Test
-    public void gameTestPuroSpare ()throws Exception {
+    public void allSpare_Score119 ()throws Exception {
         Game g= new Game();
 
         g.roll(1);
         g.roll(9);
-//11
+
         g.roll(1);
         g.roll(9);
- //22
+
         g.roll(1);
         g.roll(9);
-//33
+
         g.roll(1);
         g.roll(9);
-//44
+
         g.roll(1);
         g.roll(9);
-//55
+
         g.roll(1);
         g.roll(9);
-//66
+
         g.roll(1);
         g.roll(9);
-//77
+
         g.roll(1);
         g.roll(9);
-//88
+
         g.roll(1);
         g.roll(9);
-//99
+
         g.roll(1);
         g.roll(9);
         g.roll(10);
-//119
+
         score=g.score();
         Assert.assertEquals(score, 119);
     }
+
+    @org.junit.Test
+    public void AllZero_score0 ()throws Exception {
+        Game g= new Game();
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        score=g.score();
+        Assert.assertEquals(score, 0);
+    }
+
+    @org.junit.Test
+    public void fiveStrikesAndfiveSpares_Score210 ()throws Exception {
+        Game g= new Game();
+
+        g.roll(9);
+        g.roll(1);
+
+        g.roll(7);
+        g.roll(3);
+
+        g.roll(7);
+        g.roll(3);
+
+        g.roll(8);
+        g.roll(2);
+
+        g.roll(8);
+        g.roll(2);
+
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(10);
+        g.roll(0);
+        g.roll(0);
+
+        score=g.score();
+        Assert.assertEquals(score, 210);
+    }
+
+    @org.junit.Test
+    public void NineStrikesAndZeros_Score240 ()throws Exception {
+        Game g= new Game();
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(10);
+
+        g.roll(0);
+        g.roll(0);
+
+
+        score=g.score();
+        Assert.assertEquals(score, 240);
+    }
+
+
+    @org.junit.Test
+    public void FirtsFrame0_10SecondFrame1_1_AllnextZero_Score13 ()throws Exception {
+        Game g= new Game();
+        g.roll(0);
+        g.roll(10);
+
+        g.roll(1);
+        g.roll(1);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+        g.roll(0);
+        g.roll(0);
+
+
+        score=g.score();
+        Assert.assertEquals(score, 13);
+    }
 }
+
--- src/classes/Game.java
+++ src/classes/Game.java
@@ -6,105 +6,91 @@ package classes;
 public class Game {
 
     int[] rolls = new int[21];
-    int tiro=0;
-    int total=0;
+    int shot =0;
+    int score =0;
 
-    public Game(){
-        for (int n=0;n<21;n++)
-        {
-
-               roll(0);
-        }
-        this.tiro=0;
-    }
-
-
-    public void roll (int pins){
-        rolls[tiro++] = pins;
-        }
+     public void roll (int pins){
+        rolls[shot++] = pins;
+        if ( (shot-1)%2 ==0 && isStrike(shot-1) && shot<18)
+        shot ++;
+ }
 
 
     public int score () {
-        int indiceRoll =0;
-        //recoremos Frames del juego
-        for (int frame=0;frame<10;frame ++) {
-
-             totalPuntosFrame(indiceRoll, frame);
-            //Al tener dos roll cada frame se incrementa el indice en 2
-            indiceRoll +=2;
+        int counterRoll =0;
+        for (int counterFrame =0; counterFrame <10; counterFrame++) {
+             scoreByFrame(counterRoll, counterFrame);
+           counterRoll +=2;
         }
-        return  total;
-
+        return score;
     }
 
-    public void totalPuntosFrame(int indiceRoll, int frame) {
-        if (isStrike(indiceRoll))
-               total+=  addStrikeBono(indiceRoll, frame);
-           else if(isSpare(indiceRoll))
-               total+= addSpareBono(indiceRoll);
+    public void scoreByFrame(int indexRoll, int indexFrame) {
+        if (isStrike(indexRoll))
+               score +=  addStrikeBono(indexRoll, indexFrame);
+           else if(isSpare(indexRoll))
+               score += addSpareBono(indexRoll);
                else
-               total+= addNormalPuntos(indiceRoll);
+               score += addWithoutBono(indexRoll);
     }
 
 
     /*
-     * @param indiceRoll el indice del roll que se desea conocer si es Strike
+     * @param indexRoll el indice del roll que se desea conocer si es Strike
      */
-    public boolean isStrike(int indiceRoll){
-        return rolls[indiceRoll] ==10;
+    public boolean isStrike(int indexRoll){
+        return rolls[indexRoll] ==10;
     }
 
     /*
-    * @param indiceRoll el indice del primer roll del frame del que se desea conocer si es Spare
+    * @param indexRoll el indice del primer roll del frame del que se desea conocer si es Spare
     */
-    public boolean isSpare(int indiceRoll)
+    public boolean isSpare(int indexRoll)
     {
-        return rolls[indiceRoll] + rolls[indiceRoll +1] >=10 ;
+        return rolls[indexRoll] + rolls[indexRoll +1] >=10 ;
     }
 
-    /*
-   * @param indiceRoll el indice de roll del frame que genera el Strike
-   * @param indiceFrame el indice  del Frame del que se calculara el bono
+   /*
+   * @param indexRoll el indice de roll del frame que genera el Strike
+   * @param indexFrame el indice  del Frame del que se calculara el bono
    */
-    public  int addStrikeBono(int indiceRoll, int indiceFrame) {
-        if (isFinalframe(indiceFrame))
-            //agregamos para bono los dos ultimos tiros 20 y 21
-            return PuntosStrikeFrameConTresRolls(indiceRoll);
+    public  int addStrikeBono(int indexRoll, int indexFrame) {
+        if (isFinalframe(indexFrame))
+            return addFinalFrameWithStrike(indexRoll);
         else
-            return puntosStrikeFrameConDosRolls(indiceRoll);
+            return addFrameWithStrike(indexRoll);
     }
 
-    public int PuntosStrikeFrameConTresRolls(int indiceRoll) {
-        return 10+rolls[indiceRoll +1] +  rolls[indiceRoll +2];
+    public int addFinalFrameWithStrike(int indexRoll) {
+        return 10+rolls[indexRoll +1] +  rolls[indexRoll +2];
     }
 
-    public int puntosStrikeFrameConDosRolls(int indiceRoll) {
-        if (isStrike(rolls[indiceRoll + 2]))
-             //al ser er un Strike el siguiente Frame se tomara el primer tiro del segundo frame siguiente
-            return 10+ rolls[indiceRoll +2]+ rolls[indiceRoll +4];
+    public int addFrameWithStrike(int indexRoll) {
+        if (isStrike(rolls[indexRoll + 2]))
+            return 10+ rolls[indexRoll +2]+ rolls[indexRoll +4];
         else
-            return  10+ rolls[indiceRoll +2] + rolls[indiceRoll +3];
+            return  10+ rolls[indexRoll +2] + rolls[indexRoll +3];
     }
 
     /*
-    * @param indiceFrame el indice del Frame
+    * @param indexFrame el indice del Frame
     */
-    public boolean isFinalframe (int indiceFrame){
-        return indiceFrame ==9;
+    public boolean isFinalframe (int indexFrame){
+        return indexFrame ==9;
     }
 
     /*
-    * @param indiceRoll el indice del primer roll del Frame
+    * @param indexRoll el indice del primer roll del Frame
     */
-    public int addNormalPuntos(int indiceRoll){
-        return rolls[indiceRoll] + rolls[indiceRoll +1];
+    public int addWithoutBono(int indexRoll){
+        return rolls[indexRoll] + rolls[indexRoll +1];
     }
 
     /*
-    * @param indiceRoll el indice del primer roll del Frame genera el spare
+    * @param indexRoll el indice del primer roll del Frame genera el spare
     */
-    public  int addSpareBono(int indiceRoll){
-        return  10 +rolls[indiceRoll+2];
+    public  int addSpareBono(int indexRoll){
+        return  10 +rolls[indexRoll +2];
     }
 
 
